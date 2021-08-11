78eaa1ce2aa54ec5939be8b18f06c7ca75073a9a
Libbna <libbna260296@gmail.com>
2021-08-11 16:01:54 +0530

    add js to hamburger

--- admin/header.php
+++ admin/header.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="http://localhost/blog/assets/style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
@@ -14,7 +16,7 @@
 </head>
 
 <style>
-    
+
 </style>
 
 <body>
@@ -23,6 +25,10 @@
             <div class="navbar-header">
                 <p class="head">BlogSite</p>
             </div>
+            <input type="checkbox" id="nav_checkbox" class="nav_checkbox">
+            <label for="nav_checkbox" class="nav_toggle">
+                <img class=" menu" src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png" />
+            </label>
             <ul class="nav navbar-nav">
                 <li><a href="index.php">Dashboard</a></li>
                 <li><a href="logout.php">Logout</a></li>
--- header.php
+++ header.php
@@ -7,11 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="http://localhost/blog/assets/style.css" rel="stylesheet" type="text/css">
-    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+
     <title>Document</title>
+    <!-- <script type="text/javascript">
+        $(document).ready(function() {
+            $("#toggle").click(function() {
+                $('span').toggleClass('active')
+            })
+        })
+    </script> -->
+
 </head>
 
 
@@ -21,18 +33,39 @@
             <div class="navbar-header">
                 <p class="head">BlogSite</p>
             </div>
-            <input type="checkbox" id="nav_checkbox" class="nav_checkbox">
-            <label for="nav_checkbox" class="nav_toggle">
-                <img class=" menu" src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png" />
-            </label>
+
+            <div id="toggle">
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <span id="bar"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                    <span id="cross"><i class="fa fa-times" aria-hidden="true"></i></span>
+                </a>
+            </div>
             <!-- navbar_nav == menu -->
-            <ul class="nav navbar-nav">
+            <ul class="nav navbar-nav" id="navbar-nav">
                 <li><a href="http://localhost/blog/">Home</a></li>
                 <li><a href="http://localhost/blog/admin/login.php">Login</a></li>
                 <li><a href="http://localhost/blog/admin/register.php">Register</a></li>
             </ul>
         </div>
     </nav>
+
+    <script type="text/javascript">
+        function myFunction() {
+            var x = document.getElementById("navbar-nav");
+            var cross = document.getElementById("cross");
+            var bar = document.getElementById("bar");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+                bar.style.display = "block";
+                cross.style.display = "none"
+
+            } else {
+                x.style.display = "block";
+                bar.style.display = "none";
+                cross.style.display = "block"
+            }
+        }
+    </script>
 </body>
 
 
