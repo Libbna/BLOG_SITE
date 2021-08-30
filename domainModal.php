9e557e7de54a2a1fa4c17f071d9ebf11c16a12d9
Libbna <libbna260296@gmail.com>
2021-08-31 00:51:17 +0530

    UI of header section completed

---
+++ domainModal.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="/assets/main.css">
+</head>
+
+<body>
+    <div class="modal right fade show" id="DomainModal" tabindex="-1" role="dialog" aria-labelledby="DomainModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content right-toggle-bg">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <div class="modal-body">
+                    <div class="modal__content">
+                        <h2 class="logo">
+                            <a href="#" class="logo-brand">Publication</a>
+                        </h2>
+                        <p class="mt-md-3 mt-2">Lorem ipsum dolor sit amet, elit. Eos expedita ipsam at fugiat ab.</p>
+                        <div class="widget-menu-items mt-sm-5 mt-4">
+                            <h5 class="widget-title">Menu Items</h5>
+                            <nav class="navbar p-0">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="index.html">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">About</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Privacy Policy</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Contact</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="widget-social-icons mt-sm-5 mt-4">
+                            <h5 class="widget-title">Contact Us</h5>
+                            <ul class="icon-rounded address">
+                                <li>
+                                    <p>New Delhi, India</p>
+                                </li>
+                                <li class="mt-3">
+                                    <p>
+                                        <span class="fa fa-phone"></span>
+                                        <a href="tel:+ 8957857985">91+ 8957857985</a>
+
+                                    </p>
+                                </li>
+                                <li class="mt-2">
+                                    <p>
+                                        <span class="fa fa-envelope"></span>
+                                        <a href="mailto:blogsite@mail.com">blogsite@mail.com</a>
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="widget-social-icons mt-sm-5 mt-4">
+                            <h5 class="widget-title">Follow Us</h5>
+                            <ul class="icon-rounded address">
+                                <li>
+                                    <a href="twitter.com" target="_blank" class="social-link twitter">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="linkedin.com" target="_blank" class="social-link linkedin">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="tumblr.com" target="_blank" class="social-link tumblr">
+                                        <i class="fa fa-tumblr"></i>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"></script>
+    <!-- Main CSS -->
     <link rel="stylesheet" href="/assets/main.css">
 </head>
 
@@ -71,9 +72,9 @@
                         </div>
                         <a href="#domain" class="domain" data-toggle="modal" data-target="#DomainModal">
                             <div class="hamburger1">
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                <div class="d1"></div>
+                                <div class="d2"></div>
+                                <div class="d3"></div>
                             </div>
                         </a>
                     </div>
@@ -83,24 +84,25 @@
         </div>
     </header>
 
+    <!-- Domain Modal -->
+
+    <?php
+    include('domainModal.php');
+    ?>
 
 
-    <!-- <script src="content/js/jquery.min.js"></script>
-    <script src="content/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('.dropdown-toggle').dropdown();
-        });
-    </script> -->
 
-    <!-- jQuery library -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 
-    <!-- Popper JS -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
 
-    <!-- Latest compiled JavaScript -->
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
+
+
+
+
+
+
+
+
+
 </body>
 
 </html>
