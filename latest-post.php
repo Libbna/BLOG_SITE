692783d1dd67c308843e232562aa17185d7b8a8c
Libbna <libbna260296@gmail.com>
2021-09-05 02:18:15 +0530

    latest-post updated

--- latest-post.php
+++ latest-post.php
@@ -114,6 +114,58 @@
             </div>
         </div>
     </section>
+    <!-- Section 3 -->
+    <section class="w3l-latest-posts pt-lg-5 pt-0 pb-5">
+        <div class="container pb-lg-3">
+            <div class="row">
+                <div class="col-lg-4 latest-col">
+                    <a href="blog-single.html" class="blog-article-post height-full bg-color-three d-flex flex-wrap align-content-between">
+                        <div class="post-content">
+                            <h6 class="sub-title">July 04, 2020</h6>
+                            <h4 class="blog_post_title"> How to Identify first blog post's topic
+                            </h4>
+                            <ul class="author-date my-4 d-flex align-items-center">
+                                <li>by <b>Gwen Johnson</b></li>
+                            </ul>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto necessitatibus consectetur dolor ipsam adipisci aperiam,
+                                voluptatum ipsum harum ducimus veritatis iusto velit labore rerum quibusdam!</p>
+                        </div>
+                        <div class="read-button mt-5">Read story <span class="fa fa-long-arrow-right" aria-hidden="true"></span></div>
+                    </a>
+                </div>
+                <div class="col-lg-4 mt-lg-0 mt-4 latest-col">
+                    <a href="blog-single.html" class="blog-article-post height-full bg-color-three d-flex flex-wrap align-content-between">
+                        <div class="post-content">
+                            <h6 class="sub-title">Sep 18, 2020</h6>
+                            <h4 class="blog_post_title"> 5 ways to fix a leaky faucet
+                            </h4>
+                            <ul class="author-date my-4 d-flex align-items-center">
+                                <li>by <b>Marko Dugonji</b></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio incidunt doloribus minima minus, quidem, sed iusto alias, vero
+                                cupiditate at nobis itaque aut. Doloremque excepturi distinctio earum perspiciatis, cum quas magni ratione ut quae dolore....</p>
+                        </div>
+                        <div class="read-button mt-5">Read story <span class="fa fa-long-arrow-right" aria-hidden="true"></span></div>
+                    </a>
+                </div>
+                <div class="col-lg-4 mt-lg-0 mt-4 latest-col">
+                    <a href="blog-single.html" class="blog-article-post height-full bg-color-three d-flex flex-wrap align-content-between">
+                        <div class="post-content">
+                            <h6 class="sub-title">nov 24, 2020</h6>
+                            <h4 class="blog_post_title"> Write an intro (make it captivating).
+                            </h4>
+                            <ul class="author-date my-4 d-flex align-items-center">
+                                <li>by <b>Daniel Roberto</b></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto neque perspiciatis debitis nostrum, similique ut vel nobis explicabo
+                                ducimus ratione, aut asperiores nemo veniam. Voluptatum at hic id quis delectus!</p>
+                        </div>
+                        <div class="read-button mt-5">Read story <span class="fa fa-long-arrow-right" aria-hidden="true"></span></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
