3d4acef3533f2ced9e8a6b268a5b648c014e0f73
Libbna <libbna260296@gmail.com>
2021-09-20 17:57:07 +0530

    added base tag to te header file - to make the url relative.

--- components/blog-single.php
+++ components/blog-single.php
@@ -15,10 +15,11 @@ $row = $stmt->fetch();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <base href="http://blogsite.com/"> -->
     <title>Document</title>
 
     <!-- font awesome icon  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel=" stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- boostrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Main css -->
