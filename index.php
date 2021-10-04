df888c056e9e52eb01fc7e4aac25023046ea1e4a
Libbna <libbna260296@gmail.com>
2021-10-04 15:06:39 +0530

    #45 added scroll back to top feature

--- index.php
+++ index.php
@@ -116,6 +116,11 @@ $stmt = $db->query('SELECT * FROM article ORDER BY articleID DESC');
         </div>
     </section>
 
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+        <i class="fa fa-arrow-up"></i>
+    </button>
+
+
     <?php
     include("./layouts/latest-post.php");
     include("./layouts/about-image-grid.php");
@@ -139,6 +144,29 @@ $stmt = $db->query('SELECT * FROM article ORDER BY articleID DESC');
 
                 }
             })
+        }
+    </script>
+    <script>
+        //Get the button
+        var mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
         }
     </script>
 
