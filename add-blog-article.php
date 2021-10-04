f277fb3dbe1d8d48784e65029863b4c4f9786ae2
RChaubey16 <ruturajchaubey16@gmail.com>
2021-10-04 12:30:13 +0530

    Updation

--- add-blog-article.php
+++ add-blog-article.php
@@ -39,8 +39,6 @@ if (isset($_POST['submit'])) {
         $error[] = 'Please enter Author Name';
     }
 
-
-
     if (!isset($error)) {
         try {
             $stmt = $db->query("INSERT INTO article (articleTitle, articleDesc, articleContent, articleAuthor, profile_img) VALUES ('$articleTitle', '$articleDesc', '$articleContent', '$articleAuthor', '$destinationFile')");
