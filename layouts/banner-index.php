1d3cb0c869488c63a779e974a74f3c14c1984f6f
RChaubey16 <ruturajchaubey16@gmail.com>
2021-10-11 12:41:24 +0530

    #31: Code cleanup

--- layouts/banner-index.php
+++ layouts/banner-index.php
@@ -15,7 +15,6 @@ $result = $db->query("SELECT banner_path FROM banners");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <!-- main css -->
