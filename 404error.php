99b9440f5784816c7eb336cb81ff8b6bee9a403f
RChaubey16 <ruturajchaubey16@gmail.com>
2021-11-10 13:40:34 +0530

    #31: Removed commented code

--- 404error.php
+++ 404error.php
@@ -19,11 +19,8 @@ if ($error == 404) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <!-- font awesome icon  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- boostrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Main css -->
     <link rel="stylesheet" href="./assets/sass/utilities/main.css">
 
     <style>
